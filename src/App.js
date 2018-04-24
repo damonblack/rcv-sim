409f1e85f2b9496f85438b18a89e652709ca1693
Damon Black <dblack@envysion.com>
2018-04-24 06:04:35 -0600

    Adds candidates to elections

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import AppBar from 'material-ui/AppBar';
 import Avatar from 'material-ui/Avatar';
 import TextField from 'material-ui/TextField';
 import { List, ListItem } from 'material-ui/List';
+import Chip from 'material-ui/Chip';
+import Subheader from 'material-ui/Subheader';
+import EditorInsertChart from 'material-ui/svg-icons/editor/insert-chart';
+import Divider from 'material-ui/Divider';
+import Paper from 'material-ui/Paper';
+import { blue500 } from 'material-ui/styles/colors';
 
 import { auth, googleAuth, database } from './services';
 import './App.css';
@@ -17,6 +23,7 @@ class App extends Component {
       elections: [],
       creating: false,
       electionTitle: '',
+      candidates: [ '','' ],
     }
   }
 
@@ -61,9 +68,29 @@ class App extends Component {
     this.setState({ [fieldName]: value });
   }
 
+  handleChangeCandidate = (e) => {
+    const value = e.target.value;
+    const index = e.target.dataset.index;
+    const candidates = this.state.candidates.slice(0);
+    candidates[index] = value;
+    this.setState({ candidates });
+  }
+
+  addCandidate = () => {
+    const candidates = this.state.candidates;
+    candidates.push('');
+    this.setState({candidates});
+  }
+
   handleSubmit = () => {
     const electionsRef = database.ref(`elections/${this.state.user.uid}`);
-    electionsRef.push({ title: this.state.electionTitle });
+    const electionKey = electionsRef.push({ title: this.state.electionTitle }).key;
+    const candidatesRef = database.ref(`candidates/${this.state.user.uid}/${electionKey}`);
+
+    this.state.candidates.forEach((candidate) => {
+      candidatesRef.push(candidate);
+    });
+
     this.setState({creating: false, electionTitle: ''});
   }
 
@@ -76,8 +103,10 @@ class App extends Component {
         </AppBar>
         {this.state.user ?
           <div>
-            <span>Logged in as {user.displayName}</span>
-            <Avatar src={user.photoURL} />
+            <Chip>
+              <Avatar src={user.photoURL} />
+              {user.displayName}
+            </Chip>
             <RaisedButton onClick={this.logout}>Log Out</RaisedButton> 
           </div>
           :
@@ -89,24 +118,53 @@ class App extends Component {
 
         {this.state.creating && 
           <form onSubmit={this.handleSubmit}>
-            <TextField 
-              type="text" 
-              hintText="My Election Name" 
-              floatingLabelText="Name your election" 
-              value={electionTitle} 
-              data-name="electionTitle" 
-              onChange={this.handleChange}
-            />
-            <RaisedButton type='submit'>Submit</RaisedButton>
+            <Paper zDepth={2}>
+              <TextField
+                key={1}
+                type="text" 
+                hintText="My Election Name" 
+                floatingLabelText="Name your election" 
+                value={electionTitle} 
+                data-name="electionTitle" 
+                underlineShow={false}
+                onChange={this.handleChange}
+              />
+              <Divider />
+              <Divider />
+              { this.state.candidates.map((candidate, i) => (
+                <div>
+                  <TextField
+                    key={i + 1}
+                    type="text" 
+                    hintText="John Denver" 
+                    floatingLabelText={`Candidate ${i + 1}`}
+                    value={candidate} 
+                    data-index={i}
+                    underlineShow={false}
+                    onChange={this.handleChangeCandidate}
+                  />
+                  <Divider />
+                </div>
+              ))}
+              <RaisedButton type='button' onClick={this.addCandidate}>Add</RaisedButton>
+              <RaisedButton type='submit'>Submit</RaisedButton>
+            </Paper>
           </form>
         }
 
         {this.state.user && 
           <div className="election-list">
-            <span>My Elections</span>
             <List>
-              {this.state.elections.map((election) => 
-                <ListItem>{election.title}</ListItem>)}
+              <Subheader>Elections</Subheader>
+              {this.state.elections.map((election, i) => 
+                <ListItem 
+                  key={i}
+                  leftAvatar={
+                    <Avatar icon={<EditorInsertChart />} backgroundColor={blue500} />
+                  }
+                  primaryText={election.title}
+                />
+              )}
             </List>
           </div>
         }
