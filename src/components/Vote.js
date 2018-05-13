35a5933e49811ae2fced5a0c0df950af1c312b05
Damon Black <dblack@envysion.com>
2018-05-12 19:37:08 -0600

    Add round-to-round election resultst

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { withStyles } from 'material-ui/styles';
 import Table, {
   TableBody,
   TableHead,
   TableRow,
-  TableCell,
-} from 'material-ui/Table'; 
+  TableCell
+} from 'material-ui/Table';
 import {
   Button,
   IconButton,
+  ButtonBase,
   Typography,
   Paper,
   Snackbar
@@ -16,27 +18,31 @@ import {
 import {
   Done as CheckIcon,
   PanoramaWideAngle as EmptyIcon,
-  Close as CloseIcon
+  Close as CloseIcon,
+  Home as HomeIcon
 } from '@material-ui/icons';
 
 import { database } from '../services';
 
-
-const styles = { 
-  wrapper: { display: 'flex', alignItems: 'center', justifyContent: 'center', height: '80vh' },
+const styles = {
+  wrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '80vh'
+  },
   results: { width: '50%' },
-  positionHeader: { textAlign: 'center' },
+  positionHeader: { textAlign: 'center' }
 };
 
 class Vote extends Component {
-
   defaultState = {
     title: '',
     candidates: [],
     votes: {},
     lastAction: '',
-    notifierOpen: false,
-  }
+    notifierOpen: false
+  };
 
   constructor(props) {
     super(props);
@@ -56,35 +62,42 @@ class Vote extends Component {
   }
 
   static rankName(number) {
-    if (number === 1) return "first";
-    if (number === 2) return "second";
-    if (number === 3) return "third";
-    return number + "th";
+    if (number === 1) return 'first';
+    if (number === 2) return 'second';
+    if (number === 3) return 'third';
+    return number + 'th';
   }
 
   static actionText(candidateName, displacedName, position, previousPosition) {
-    const replacing = displacedName === "" ? "" : `, replacing ${displacedName}`;
-    const goFirstPlace = position === 1 ? ` Go ${candidateName}!` : "";
+    const replacing =
+      displacedName === '' ? '' : `, replacing ${displacedName}`;
+    const goFirstPlace = position === 1 ? ` Go ${candidateName}!` : '';
 
-    if (!previousPosition) return `You've chosen ${candidateName} as your ${Vote.rankName(position)} choice${replacing}.${goFirstPlace}`;
+    if (!previousPosition)
+      return `You've chosen ${candidateName} as your ${Vote.rankName(
+        position
+      )} choice${replacing}.${goFirstPlace}`;
 
-    const verb = (previousPosition > position) ? "promoting" : "demoting";
+    const verb = previousPosition > position ? 'promoting' : 'demoting';
 
-    return `You've changed your vote for ${candidateName}, ${verb} them to your ${Vote.rankName(position)} choice${replacing}.`;
+    return `You've changed your vote for ${candidateName}, ${verb} them to your ${Vote.rankName(
+      position
+    )} choice${replacing}.`;
   }
 
   componentDidMount() {
     const electionKey = this.props.match.params.key;
-    Vote.electionRef(electionKey).on('value', (snapshot) => {
+    Vote.electionRef(electionKey).on('value', snapshot => {
       const election = snapshot.val();
       this.setState({ title: election.title });
     });
 
-    Vote.candidatesRef(electionKey).on('value', (snapshot) => {
+    Vote.candidatesRef(electionKey).on('value', snapshot => {
       const candidatesVal = snapshot.val();
-      const candidates = Object.keys(candidatesVal).map((key) => (
-        { id: key, name: candidatesVal[key].name}
-      ));
+      const candidates = Object.keys(candidatesVal).map(key => ({
+        id: key,
+        name: candidatesVal[key].name
+      }));
 
       this.setState({ candidates });
     });
@@ -94,13 +107,15 @@ class Vote extends Component {
     if (this.state.votes[position] === candidateId) return;
     const votes = Object.assign({}, this.state.votes);
 
-    const candidateName = this.state.candidates.filter(c => c.id == candidateId)[0].name;
-    const displacedName = votes[position] ?
-      this.state.candidates.filter(c => c.id == votes[position])[0].name
-      : "";
+    const candidateName = this.state.candidates.filter(
+      c => c.id == candidateId
+    )[0].name;
+    const displacedName = votes[position]
+      ? this.state.candidates.filter(c => c.id == votes[position])[0].name
+      : '';
 
     let previousPosition;
-    Object.keys(votes).forEach((key) => {
+    Object.keys(votes).forEach(key => {
       if (votes[key] === candidateId) {
         previousPosition = key;
         votes[key] = null;
@@ -108,67 +123,85 @@ class Vote extends Component {
     });
     votes[position] = candidateId;
 
-    const lastAction = Vote.actionText(candidateName, displacedName, position, previousPosition);
+    const lastAction = Vote.actionText(
+      candidateName,
+      displacedName,
+      position,
+      previousPosition
+    );
 
     this.setState({ votes, lastAction, notifierOpen: true });
-  }
+  };
 
   submitVote = () => {
     const electionKey = this.props.match.params.key;
     Vote.votesRef(electionKey).push(this.state.votes);
     this.setState({ votes: {} });
-  }
+  };
 
   closeNotifier = () => this.setState({ notifierOpen: false });
-  
+
   render() {
     const { title, candidates, notifierOpen, lastAction } = this.state;
     const { classes } = this.props;
 
     return (
       <div className={classes.wrapper}>
+        <ButtonBase component={Link} to="/">
+          <HomeIcon color="secondary" />
+        </ButtonBase>
         <div className={classes.voting}>
           <Typography variant="headline">{title}</Typography>
           <Paper>
             <Table>
               <TableHead>
                 <TableRow>
-                  <TableCell key={0} colSpan="2"/>
+                  <TableCell key={0} colSpan="2" />
                   {candidates.map((candidate, i) => (
-                    <TableCell key={i + 1} className={classes.positionHeader}>{i + 1}</TableCell>
+                    <TableCell key={i + 1} className={classes.positionHeader}>
+                      {i + 1}
+                    </TableCell>
                   ))}
                 </TableRow>
               </TableHead>
               <TableBody>
                 {candidates.map((candidate, i) => (
                   <TableRow key={i}>
-                    <TableCell key={0} colSpan="2">{candidate.name}</TableCell>
-                    { candidates.map((c, i) => (
+                    <TableCell key={0} colSpan="2">
+                      {candidate.name}
+                    </TableCell>
+                    {candidates.map((c, i) => (
                       <TableCell key={i + 1}>
-                        <IconButton onClick={(e) => this.updateVote(candidate.id, i + 1)}>
-                          {this.state.votes[i + 1] === candidate.id ?
-                            <CheckIcon /> : <EmptyIcon />
-                          }
+                        <IconButton
+                          onClick={e => this.updateVote(candidate.id, i + 1)}
+                        >
+                          {this.state.votes[i + 1] === candidate.id ? (
+                            <CheckIcon />
+                          ) : (
+                            <EmptyIcon />
+                          )}
                         </IconButton>
                       </TableCell>
                     ))}
-                  </TableRow> 
+                  </TableRow>
                 ))}
               </TableBody>
             </Table>
           </Paper>
-          <Button variant="raised" color="secondary" onClick={this.submitVote}>Submit Vote</Button>
+          <Button variant="raised" color="secondary" onClick={this.submitVote}>
+            Submit Vote
+          </Button>
         </div>
         <Snackbar
           anchorOrigin={{
             vertical: 'bottom',
-            horizontal: 'center',
+            horizontal: 'center'
           }}
           open={notifierOpen}
           autoHideDuration={4000}
           onClose={this.closeNotifier}
           SnackbarContentProps={{
-            'aria-describedby': 'message-id',
+            'aria-describedby': 'message-id'
           }}
           message={<span id="message-id">{lastAction}</span>}
           action={[
@@ -180,9 +213,9 @@ class Vote extends Component {
               onClick={this.closeNotifier}
             >
               <CloseIcon />
-            </IconButton>,
+            </IconButton>
           ]}
-         />
+        />
       </div>
     );
   }
---
+++ src/lib/voteCounter.test.js
@@ -0,0 +1,44 @@
+import { getResults } from './voteCounter';
+
+const earlyWin = [
+  { 1: 'D', 2: 'B', 3: 'C', 4: 'A', 5: 'E' },
+  { 1: 'D', 2: 'C', 3: 'B', 4: 'A', 5: 'E' },
+  { 1: 'D', 2: 'E', 3: 'C', 4: 'A', 5: 'B' },
+  { 1: 'C', 2: 'B', 3: 'A', 4: 'D', 5: 'E' },
+  { 1: 'E', 2: 'B', 3: 'C', 4: 'D', 5: 'A' },
+  { 1: 'A', 2: 'D', 3: 'C', 4: 'B', 5: 'E' },
+  { 1: 'D', 2: 'A', 3: 'C', 4: 'E', 5: 'B' }
+];
+
+const balanced = [
+  { 1: 'B', 2: 'D', 3: 'C', 4: 'A', 5: 'E' },
+  { 1: 'D', 2: 'C', 3: 'B', 4: 'A', 5: 'E' },
+  { 1: 'E', 2: 'A', 3: 'C', 4: 'D', 5: 'B' },
+  { 1: 'C', 2: 'B', 3: 'A', 4: 'D', 5: 'E' },
+  { 1: 'E', 2: 'B', 3: 'C', 4: 'D', 5: 'A' },
+  { 1: 'A', 2: 'D', 3: 'C', 4: 'B', 5: 'E' },
+  { 1: 'D', 2: 'A', 3: 'C', 4: 'E', 5: 'B' }
+];
+
+const candidates = ['A', 'B', 'C', 'D', 'E'];
+
+it('should run only round 0 and return results', () => {
+  const results = getResults(earlyWin, candidates);
+
+  expect(results.length).toEqual(1);
+  expect(results[0].winner).toEqual('D');
+  expect(results).toMatchSnapshot('early win');
+});
+
+it('should multiple rounds with the winner on the last only', () => {
+  const results = getResults(balanced, candidates);
+  const firstRound = results[0];
+  const secondRound = results[1];
+  const lastRound = results[results.length - 1];
+
+  expect(results.length).toEqual(3);
+  expect(firstRound.winner).toBeUndefined();
+  expect(secondRound.winner).toBeUndefined();
+  expect(lastRound.winner).toEqual('D');
+  expect(results).toMatchSnapshot('balanced win');
+});
