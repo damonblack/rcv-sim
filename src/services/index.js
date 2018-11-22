df3157a23d6655bfe73beb3538ee6b9166e886bd
Damon Black <dblack@envysion.com>
2018-11-21 23:21:14 -0700

    Update Google auth config for new rcv-tally-app

--- src/services/index.js
+++ src/services/index.js
@@ -2,12 +2,20 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyCHtzTb8Fmgy1tu3tQ2ghmaTH_1lhgr6s4',
-  authDomain: 'rcv-sim.firebaseapp.com',
-  databaseURL: 'https://rcv-sim.firebaseio.com',
-  projectId: 'rcv-sim',
-  storageBucket: 'rcv-sim.appspot.com',
-  messagingSenderId: '710177801325'
+  apiKey: 'AIzaSyAUsa3h0epPy2uvmLHEQIm0GyO0FKhb1vU',
+  authDomain: 'rcv-tally-app.firebaseapp.com',
+  databaseURL: 'https://rcv-tally-app.firebaseio.com',
+  projectId: 'rcv-tally-app',
+  storageBucket: 'rcv-tally-app.appspot.com',
+  messagingSenderId: '867195006035'
+
+  // config info for rcv-sim.firebaseapp.com
+  // apiKey: 'AIzaSyCHtzTb8Fmgy1tu3tQ2ghmaTH_1lhgr6s4',
+  // authDomain: 'rcv-sim.firebaseapp.com',
+  // databaseURL: 'https://rcv-sim.firebaseio.com',
+  // projectId: 'rcv-sim',
+  // storageBucket: 'rcv-sim.appspot.com',
+  // messagingSenderId: '710177801325'
 };
 
 firebase.initializeApp(config);
